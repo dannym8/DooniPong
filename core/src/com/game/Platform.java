b1430f4b2d5684f7b103a98c5b10399c153b3d48
Danny m8 <daniel.glinberg@gmail.com>
2022-08-27 22:44:40 -0400

    Finished delegating

--- core/src/com/game/Platform.java
+++ core/src/com/game/Platform.java
@@ -1,5 +1,27 @@
 package com.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+import static com.game.PongGame.PLATFORM_HEIGHT;
+import static com.game.PongGame.PLATFORM_WIDTH;
+
 public class Platform {
 
+    ShapeRenderer.ShapeType type;
+    Color color;
+
+    public Platform(ShapeRenderer.ShapeType type, Color color) {
+        this.type = type;
+        this.color = color;
+    }
+
+    public void renderPlatform(ShapeRenderer renderer) {
+        renderer.begin(type);
+        renderer.setColor(color);
+        int mouseX = Gdx.input.getX();
+        renderer.rect(mouseX - (PLATFORM_WIDTH/2), 20, PLATFORM_WIDTH, PLATFORM_HEIGHT);
+        renderer.end();
+    }
 }
--- core/src/com/game/PongGame.java
+++ core/src/com/game/PongGame.java
@@ -3,6 +3,7 @@ package com.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -19,6 +20,7 @@ public class PongGame extends ApplicationAdapter {
 	private ArrayList<Tile> tiles;
 	private TileGrid tileGrid;
 	private Background bg;
+	private Platform platform;
 
 	public static final float PLATFORM_WIDTH = 83;
 	public static final float PLATFORM_HEIGHT = 19;
@@ -44,28 +46,21 @@ public class PongGame extends ApplicationAdapter {
 
 		ball = new Ball(camera.viewportWidth/2,camera.viewportHeight/2,9,3,3);
 
+		platform = new Platform(ShapeRenderer.ShapeType.Filled, Color.WHITE);
+
 	}
 
 	public void render () {
 		// render background image
 		bg.renderBackground(batch);
 		// render player platform (paddle)
-		renderPlatform();
+		platform.renderPlatform(renderer);
 		// draw tile grid
 		tileGrid.drawGrid(renderer);
 		// update ball position (movement)
 		ball.updatePos();
 		// render ball
 		ball.renderBall(renderer);
-	}
-
-	// renders the player platform
-	private void renderPlatform() {
-		renderer.begin(ShapeRenderer.ShapeType.Filled);
-		renderer.setColor(255,50,255,100);
-		int mouseX = Gdx.input.getX();
-		renderer.rect(mouseX - (PLATFORM_WIDTH/2), 20, PLATFORM_WIDTH, PLATFORM_HEIGHT);
-		renderer.end();
 	}
 
 	@Override
